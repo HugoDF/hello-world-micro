235aa50d5b4a5d5cca8f4436e89bd040004968e5
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-09-04 18:42:09 +0100

    enable CORS

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-module.exports = () => 'Hello world';
+const enableCors = fn => (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  return fn();
+}
+module.exports = enableCors(() => 'Hello world');
